4f1c4169caea21d3d571082c0577bce45cc84bf1
Kawsar <diu.kawsar@gmail.com>
2025-04-27 10:41:24 +0600

    country model reference fix

--- lib/country.dart
+++ lib/country.dart
@@ -1,22 +1,27 @@
 part of 'country_list.dart';
 
-class InternalCountry {
+class _InternalCountry {
   final String name;
   final String code;
 
-  const InternalCountry({required this.name, required this.code});
+  const _InternalCountry({required this.name, required this.code});
 
-  factory InternalCountry.fromJson(Map<String, dynamic> json) {
-    return InternalCountry(
-      name: json['name'],
-      code: json['code']
-    );
+  Country toCountry({required ImageQuality quality}) {
+    return Country._(getFlag(quality, code), name: name, code: code);
+  }
+
+  String getFlag(ImageQuality quality, String code) {
+    final subdir = quality == ImageQuality.small ? "_80" : "_160";
+    return "assets/$subdir/$code.png";
   }
 }
 
 class Country {
   final String name;
   final String code;
-  final String flag;
-  const Country({required this.name, required this.code, required this.flag});
+  final String _flag;
+
+  const Country._(this._flag,{required this.name, required this.code});
+
+  AssetImage get flag => AssetImage(_flag, package: "country_list");
 }
--- lib/country_list.dart
+++ lib/country_list.dart
@@ -5,10 +5,10 @@ part 'data.dart';
 part 'sorter.dart';
 
 typedef CountryItemBuilder =
-    Widget Function(BuildContext context, InternalCountry country);
+    Widget Function(BuildContext context, Country country);
 
 class CountryListView extends StatefulWidget {
-  final void Function(InternalCountry country)? onSelect;
+  final void Function(Country country)? onSelect;
   final CountryItemBuilder? itemBuilder;
   final OrderBy sortBy;
   final ImageQuality quality;
@@ -26,19 +26,14 @@ class CountryListView extends StatefulWidget {
 }
 
 class _CountryListViewState extends State<CountryListView> {
-  late List<InternalCountry> countries;
-
-  AssetImage getFlag(ImageQuality quality, String code) {
-    final subdir = quality == ImageQuality.small ? "_80" : "_160";
-    return AssetImage("assets/$subdir/$code.png", package: "country_list");
-  }
+  late List<_InternalCountry> countries;
 
   @override
   void initState() {
     super.initState();
     countries = switch (widget.sortBy) {
-      OrderBy.name => sovereignCountries,
-      OrderBy.code => List<InternalCountry>.from(sovereignCountries)
+      OrderBy.name => _sovereignCountries,
+      OrderBy.code => List<_InternalCountry>.from(_sovereignCountries)
         ..sort((a, b) => a.code.compareTo(b.code)),
     };
   }
@@ -48,8 +43,7 @@ class _CountryListViewState extends State<CountryListView> {
     return ListView.builder(
       itemCount: countries.length,
       itemBuilder: (context, index) {
-        final country = countries[index];
-        final flag = getFlag(widget.quality, country.code);
+        final country = countries[index].toCountry(quality: widget.quality);
         if (widget.itemBuilder != null) {
           return GestureDetector(
             onTap: () => widget.onSelect?.call(country),
@@ -60,7 +54,7 @@ class _CountryListViewState extends State<CountryListView> {
           );
         }
         return ListTile(
-          leading: Image(image: flag, width: 32),
+          leading: Image(image: country.flag, width: 32),
           title: Text(country.name),
           onTap: () => widget.onSelect?.call(country),
         );
